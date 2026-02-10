06d14394483826ed403764c90e7ac257af8b4b48
Rajarshi <rajarshiraha007@gmail.com>
2026-02-11 01:55:26 +0530

    Made a small change in Css for mobile applications

--- frontend/expense-tracker/src/pages/auth/Signup.jsx
+++ frontend/expense-tracker/src/pages/auth/Signup.jsx
@@ -70,7 +70,7 @@ const Signup = () => {
         <form onSubmit={handleSignup}>
           <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
 
-          <div className='grid grid-cols-1 md:grid-col-2 gap-4'>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
             <Input
             value={fullName}
             onChange={({target}) => setFullName(target.value)}
